299877393600513b5e5dab593c32927af80afb70
Joshua <joshuahuang334@gmail.com>
2022-07-15 15:31:29 +0000

    Fuzztest

--- utils/buffer/fuzz-for-30-sec.sh
+++ utils/buffer/fuzz-for-30-sec.sh
@@ -1,24 +1,26 @@
 #! /usr/bin/sh
 #
 #
-# Used in GitHub actions ot fuzz utils/buffer for 30s, to determine if there are
+# Used in GitHub actions to fuzz utils/buffer for 30s, to determine if there are
 # any immediate errors. This is only ran for 30s because that fuzzing can take
 # awhile, and waiting on GitHub actions isn't optimal
 #
 # This script does the following:
 #   (1) sets the Rust default to nightly
-#   (1) runs rust fuzz test on utils/buffer for 30 seconds
+#   (2) navigates to correct directory to run fuzztests
+#   (3) runs rust fuzz test on utils/buffer for 30 seconds
+#   (4) if fuzz test fails before 30s runtime, the script exits indicating Failure.
+#       Otherwise indicates success.
 #
 # This script is called by `.github/workflows/fuzz-utils-buffer.yaml` on every PR onto the main branch.
 #
 
-echo "Script is starting"
-# rustup override set nightly
-# cd ./utils/buffer/fuzz
-# cargo install cargo-fuzz
-# cargo +nightly fuzz run faster -- -rss_limit_mb=5000000000
+echo "Fuzztest script is starting"
+rustup override set nightly
+cd fuzz
+cargo install cargo-fuzz
 
-if ! timeout 10s sleep 12s
+if ! timeout 30s cargo +nightly fuzz run faster -- -rss_limit_mb=5000000000
 then
     echo "Success. Fuzz tests pass"
     sleep 3s
@@ -28,3 +30,4 @@ else
     sleep 3s
     exit -1
 fi
+
